cad24fa606f6d951acb7b04538e5ba41ffdc3654
TheMaxMur <muravjev.mak@yandex.ru>
2025-02-11 01:57:16 +0300

    flake: Init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "OpenSSL GOST Nix";
+
+  inputs = {
+    # Official NixOS repo
+    unstable = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    # Current nixpkgs branch
+    nixpkgs = {
+      follows = "unstable";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { flake-parts, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = { pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            allowBroken = true;
+          };
+        };
+
+      };
+    };
+}
